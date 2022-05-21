682bcc8a2736d5e23f74bec67b789d4e6b3ecb65
mgalal2 <mgalal2@its.jnj.com>
2022-05-21 19:32:34 +0100

    Final

--- scripts/publication_results/read_cd_result.py
+++ scripts/publication_results/read_cd_result.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import json
 import os
 from enum import Enum
+import csv
 
 COLOR_CONSTS = {1: 'grey', 2: 'green', 0: 'red'}
 MARKER_CONSTS = {1: '.', 2: '+', 0: 'x'}
@@ -26,7 +27,6 @@ def read_versions(platform):
                 # marker, color = '.', 'grey' if version == 1 else '+', 'green'
                 result.append([count, version, data[key]['client_info']['elapsed_time']])
                 count += 1
-                print(count, idx)
     return result
 
 def load_data(platform, idx):
@@ -46,18 +46,30 @@ fig.text(0.5, 0.00, 'Function Invocations', ha='center')
 fig.text(0.06, 0.5, 'Execution in seconds', va='center', rotation='vertical')
 
 axes[cnt] = plt.subplot(1,2,cnt+1)
-print([x[0] for x in openwhisk])
 o_x[cnt] = [x[0] for x in openwhisk ]
 o_y[cnt] = [x[2] for x in openwhisk ]
 markers = [MARKER_CONSTS[x[1]] for x in openwhisk ]
 colors = [ COLOR_CONSTS[x[1]]  for x in openwhisk ]
-# marker = ('.', 'grey') if v == 1 else ('+', 'green')
 for x, y, c, m in zip(o_x[cnt],o_y[cnt], colors, markers):
     axes[cnt].scatter(x, y, alpha=0.8, c=c, marker=m, )  
 
 
-axes[cnt].set_title('Openwhisk')
+axes[cnt].set_title('OpenWhisk')
 axes[cnt].set_ylim([0,20])
+results = zip(o_x[cnt],o_y[cnt], colors, markers)
+grey_count = 0
+green_count = 0 
+red_count = 0 
+for x, y, c, m in results:
+    if c == 'grey':
+        grey_count +=1
+
+    elif c == 'green':
+        green_count += 1
+    else:
+        red_count += 1
+print(grey_count, green_count, red_count)
+
 
 cnt += 1
 axes[cnt] = plt.subplot(1,2,cnt+1)
@@ -68,7 +80,20 @@ markers = [MARKER_CONSTS[x[1]] for x in nanos ]
 colors = [ COLOR_CONSTS[x[1]]  for x in nanos ]
 for x, y, c, m in zip(o_x[cnt],o_y[cnt], colors, markers):
     axes[cnt].scatter(x, y, alpha=0.8, c=c, marker=m, )  
-axes[cnt].set_title('Nanos')
+axes[cnt].set_title('NanoFaaS')
 plt.savefig('cd', bbox_inches='tight')
+results = zip(o_x[cnt],o_y[cnt], colors, markers)
+grey_count = 0
+green_count = 0 
+red_count = 0 
+for x, y, c, m in results:
+    if c == 'grey':
+        grey_count +=1
+
+    elif c == 'green':
+        green_count += 1
+    else:
+        red_count += 1
+print(grey_count, green_count, red_count)
 # plt.show()
 # print(nanos)
